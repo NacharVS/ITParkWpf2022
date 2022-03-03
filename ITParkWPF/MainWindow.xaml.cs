4499e429ceb8352c1c46ec408072e1c1d204ae50
nacharvs <r3dinc@yandex.ru>
2022-03-03 20:45:50 +0300

    listBox

--- ITParkWPF/MainWindow.xaml.cs
+++ ITParkWPF/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace ITParkWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+       string bufLogin = "Login";
         public MainWindow()
         {
             InitializeComponent();
@@ -28,8 +29,43 @@ namespace ITParkWPF
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             User.AddToDB(txtLogin.Text, txtName1.Text, txtEmail.Text, txtPhone.Text);
+            listLogin.Items.Add(txtLogin.Text);
             MessageBox.Show($"User {txtLogin.Text} has registered");
         }
 
+        private void txtLogin_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (bufLogin != "Login")
+            {
+                return;
+            }
+            else
+                txtLogin.Clear();
+        }
+
+        private void txtLogin_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bufLogin = txtLogin.Text;
+        }
+
+        private void txtLogin_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtLogin_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void listLogin_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
