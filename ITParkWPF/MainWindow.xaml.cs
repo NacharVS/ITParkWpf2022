020670d4132ce808b60b93f8979db254e1b38c38
nacharvs <r3dinc@yandex.ru>
2022-03-03 19:43:23 +0300

    add mongo driver

--- ITParkWPF/MainWindow.xaml.cs
+++ ITParkWPF/MainWindow.xaml.cs
@@ -20,87 +20,9 @@ namespace ITParkWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        static double a = 0;
-        static double b = 0;
-
-        static string label = "0";
-        static string sign;
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            if(label == "0")
-            {
-                label = "1";
-                lblNumber.Content = label;
-            }
-            else
-            {
-                label = "1";
-                lblNumber.Content += label;
-            }
-        }
-
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
-            if (label == "0")
-            {
-                label = "2";
-                lblNumber.Content = label;
-            }
-            else
-            {
-                label = "2";
-                lblNumber.Content += label;
-            }
-        }
-
-        private void Button_Click_2(object sender, RoutedEventArgs e)
-        {
-            sign = "+";
-            a = Convert.ToInt32(lblNumber.Content);
-            lblNumber.Content = "0";
-            label = "0";
-        }
-
-        private void Button_Click_3(object sender, RoutedEventArgs e)
-        {
-            switch (sign)
-            {
-                case "+":
-                    b = Convert.ToInt32(lblNumber.Content);
-                    a = a + b;
-                    lblNumber.Content = a.ToString();
-                    break;
-                case "-":
-                    b = a - Convert.ToInt32(lblNumber.Content);
-                    lblNumber.Content = b.ToString();
-                    break;
-            }
-        }
-
-        private void Button_Click_4(object sender, RoutedEventArgs e)
-        {
-            sign = "-";
-            a = Convert.ToInt32(lblNumber.Content);
-            lblNumber.Content = "0";
-            label = "0";
-        }
-
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            if (Convert.ToInt32(lblNumber.Content) / 10 == 0)
-            {
-                lblNumber.Content = "0";
-                label = "0";
-            }
-            else
-            {
-                lblNumber.Content = ((Convert.ToInt32(lblNumber.Content)) / 10).ToString();
-            }
-        }
+        }      
     }
 }
---
+++ ITParkWPF/User.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITParkWPF
+{
+    class User
+    {
+        public User(string name, string login, string email, string phoneNumber)
+        {
+            Name = name;
+            Login = login;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
