617eaa9409e499ede3466df528b3cf8219ef2cc4
olgaptrova <olgaptrova@gmail.com>
2022-03-13 18:31:52 +0300

    Manual Add teamplayers. HW 25

--- ITParkWPF/MainWindow.xaml.cs
+++ ITParkWPF/MainWindow.xaml.cs
@@ -20,97 +20,302 @@ namespace ITParkWPF
     /// </summary>
     public partial class MainWindow : Window
     {
-        static double a = 0;
-        static double b = 0;
+        string bufLogin = "Login";
+        string bufName = "Name";
+        string bufEmail = "E-mail";
+        string bufPhone = "Phone";
+
+        User user = new User("Name", "Login", "E-Mail", "Phone");
+
+        string[] teamList = new string[5] { "#1", "#2", "#3", "#4", "#5" };
+        List<string> buffer = User.GetLoginList();
         public MainWindow()
         {
             InitializeComponent();
-        }
-
-        private void bigbtn_Click(object sender, RoutedEventArgs e)
-        {
-            //a = a + int.Parse(txt1.Text);
-            lblMessage.Content = (int.Parse(txt1.Text) + int.Parse(txt2.Text)).ToString();
-        }
-
-        private void lbl1_MouseMove(object sender, MouseEventArgs e)
-        {
-            lbl1.IsEnabled = !lbl1.IsEnabled;
-        }
-
-        private void lbl1_MouseEnter(object sender, MouseEventArgs e)
-        {
-            lbl1.IsEnabled = !lbl1.IsEnabled;
-        }
-
-        private void txt1_GotFocus(object sender, RoutedEventArgs e)
-        {
-            txt1.Clear();
-        }
-
-        private void txt2_GotFocus(object sender, RoutedEventArgs e)
-        {
-            txt2.Clear();
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            lblMessage.Content = (a + b).ToString();
+            User.AddToDB(txtLogin.Text, txtName1.Text, txtEmail.Text, txtPhone.Text);
+            listLogin.ItemsSource = User.GetLoginList();
+            MessageBox.Show($"User {txtLogin.Text} has registered");
         }
 
-        private void txt1_TextChanged(object sender, TextChangedEventArgs e)
+        private void txtLogin_GotFocus(object sender, RoutedEventArgs e)
         {
-            if (txt1.Text == "First Number")
+            if (bufLogin != "Login")
+            {
                 return;
-            if (txt1.Text == "" || txt1.Text == null)
-                return;
-            a = double.Parse(txt1.Text);
+            }
+            else
+                txtLogin.Clear();
         }
 
-        private void txt2_TextChanged(object sender, TextChangedEventArgs e)
+        private void txtLogin_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (txt2.Text == "Second Number" || txt2.Text == "" || txt2.Text == null)
-                return;
-            b = double.Parse(txt2.Text);
+            bufLogin = txtLogin.Text;
+        }
+
+        private void txtLogin_TextInput(object sender, TextCompositionEventArgs e)
+        {
+
+        }
+
+        private void txtLogin_SourceUpdated(object sender, DataTransferEventArgs e)
+        {
+            cmbbox1.SelectedItem.ToString();
         }
 
         private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            lblMessage.Content = a - b;
+            if (listLogin.SelectedItem == null)
+                MessageBox.Show("Check user to update");
+            else
+                User.ReplaseUser(listLogin.SelectedItem.ToString(), new User(txtLogin.Text, txtName1.Text, txtEmail.Text, txtPhone.Text));
+            listLogin.ItemsSource = User.GetLoginList();
+        }
+
+        private void listLogin_Loaded(object sender, RoutedEventArgs e)
+        {
+            listLogin.ItemsSource = User.GetLoginList();
+        }
+
+        private void listLogin_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listLogin.SelectedIndex == -1)
+            {
+                return;
+            }
+            else
+            {
+                txtLogin.Text = User.GetUser(listLogin.SelectedItem.ToString()).Login;
+                txtName1.Text = User.GetUser(listLogin.SelectedItem.ToString()).Name;
+                txtEmail.Text = User.GetUser(listLogin.SelectedItem.ToString()).Email;
+                txtPhone.Text = User.GetUser(listLogin.SelectedItem.ToString()).PhoneNumber;
+
+            }
+        }
+
+        private void listLogin_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
         }
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            lblMessage.Content = (a * b).ToString();
+            
+            if (cmbbox1.SelectedIndex == 0)
+            {
+                if(teammate1.Content.ToString() == "#1 ")
+                {
+                    if (buffer.Exists(x => x == listLogin.SelectedItem.ToString()))
+                    {
+                        teammate1.Content = listLogin.SelectedItem;
+                        buffer.Remove(listLogin.SelectedItem.ToString());
+
+                    }
+                    else MessageBox.Show("Current teammate is already in team");
+                    
+                }
+                
+                else if (teammate2.Content.ToString() == "#2 ")
+                {
+
+                    if (buffer.Exists(x => x == listLogin.SelectedItem.ToString()))
+                    {
+                        teammate2.Content = listLogin.SelectedItem.ToString();
+                        buffer.Remove(listLogin.SelectedItem.ToString());
+
+                    }
+                    else MessageBox.Show("Current teammate is already in team");
+                }
+                else if (teammate3.Content.ToString() == "#3 ")
+                {
+
+                    if (buffer.Exists(x => x == listLogin.SelectedItem.ToString()))
+                    {
+                        teammate3.Content = listLogin.SelectedItem.ToString();
+                        buffer.Remove(listLogin.SelectedItem.ToString());
+
+                    }
+                    else MessageBox.Show("Current teammate is already in team");
+                }
+                else if (teammate4.Content.ToString() == "#4 ")
+                {
+
+                    if (buffer.Exists(x => x == listLogin.SelectedItem.ToString()))
+                    {
+                        teammate4.Content = listLogin.SelectedItem.ToString();
+                        buffer.Remove(listLogin.SelectedItem.ToString());
+
+                    }
+                    else MessageBox.Show("Current teammate is already in team");
+                }
+                else if (teammate5.Content.ToString() == "#5 ")
+                {
+
+                    if (buffer.Exists(x => x == listLogin.SelectedItem.ToString()))
+                    {
+                        teammate5.Content = listLogin.SelectedItem.ToString();
+                        buffer.Remove(listLogin.SelectedItem.ToString());
+
+                    }
+                    else MessageBox.Show("Current teammate is already in team");
+                }
+
+            }
+            
+            if (cmbbox1.SelectedIndex == 1)
+            {
+                Random rnd = new Random();
+                for (int i = 0; i < teamList.Length; i++)
+                {
+                    teamList[i] = buffer[rnd.Next(0, buffer.Count)];
+                    buffer.Remove(teamList[i]);
+                }
+                teammate1.Content = "#1 " + teamList[0];
+                teammate2.Content = "#2 " + teamList[1];
+                teammate3.Content = "#3 " + teamList[2];
+                teammate4.Content = "#4 " + teamList[3];
+                teammate5.Content = "#5 " + teamList[4];
+                buffer = User.GetLoginList();
+            }
+            if (cmbbox1.SelectedIndex == 2)
+            {
+                teammate1.Content = "#1 ";
+                teammate2.Content = "#2 ";
+                teammate3.Content = "#3 ";
+                teammate4.Content = "#4 ";
+                teammate5.Content = "#5 ";
+                buffer = User.GetLoginList();
+            }
+        }
+
+        private void ListBox_Loaded(object sender, RoutedEventArgs e)
+        {
+            teamListBox.ItemsSource = Team.GetTeamList();
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
         {
-            if(b == 0)
+            if (teamListBox.Items.Contains(teamname.Text))
             {
-                MessageBox.Show("Division by zero is impossible!");
-                
+                MessageBox.Show("Team alredy exists");
+            }
+            else if (teammate1.Content == "#1 " || teammate2.Content == "#2 " || teammate3.Content == "#3 " || teammate4.Content == "#4 " || teammate5.Content == "#5 ")
+            {
+                MessageBox.Show("The team is not completed");
+            }
+            else if (teamname.Text == "TeamName")
+            {
+                MessageBox.Show("Enter the name of team");
             }
             else
-            lblMessage.Content = (a / b).ToString();
+            {
+                Team.AddTeamToDB(new Team(teamname.Text, teammate1.Content.ToString(), teammate2.Content.ToString(), teammate3.Content.ToString(), teammate4.Content.ToString(), teammate5.Content.ToString()));
+                teamListBox.ItemsSource = Team.GetTeamList();
+                teamname.Clear();
+                teammate1.Content = "#1 ";
+                teammate2.Content = "#2 ";
+                teammate3.Content = "#3 ";
+                teammate4.Content = "#4 ";
+                teammate5.Content = "#5 ";
+                buffer = User.GetLoginList();
+            }
+
+        }
+
+        private void teamListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            teamListBox.ItemsSource = Team.GetTeamList();
+            Team team = Team.GetTeam(teamListBox.SelectedItem.ToString());
+            teamname.Text = team.TeamName;        
+
+            teammate1.Content = team.Teammate1;
+            teammate2.Content = team.Teammate2;
+            teammate3.Content = team.Teammate3;
+            teammate4.Content = team.Teammate4;
+            teammate5.Content = team.Teammate5;
+
+        }
+
+        private void txtName1_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (bufName != "Name")
+            {
+                return;
+            }
+            else
+                txtName1.Clear();
+        }
+
+        private void txtName1_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bufName = txtName1.Text;
+        }
+
+        private void txtEmail_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (bufEmail != "E-mail")
+            {
+                return;
+            }
+            else
+                txtEmail.Clear();
+        }
+
+        private void txtEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bufEmail = txtEmail.Text;
+        }
+
+        private void txtPhone_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            bufPhone = txtPhone.Text;
+        }
+
+        private void txtPhone_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (bufPhone != "Phone")
+            {
+                return;
+            }
+            else
+                txtPhone.Clear();
+        }
+
+        private void cmbbox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            buffer = User.GetLoginList();
         }
 
         private void Button_Click_4(object sender, RoutedEventArgs e)
         {
-            if (b == 0)
+            if (teamListBox.SelectedIndex == -1)
+                MessageBox.Show("Choose the team!!!");
+
+            else
             {
-                MessageBox.Show("Division by zero is impossible!");
+                if (Team.GetTeamList().Exists(x => x == teamname.Text))
+                {
+                    MessageBox.Show("Team already exists in database");
+                }
+                else
+                {
+                    Team.ReplaceTeamToDB(new Team(teamname.Text, teammate1.Content.ToString(), teammate2.Content.ToString(), teammate3.Content.ToString(), teammate4.Content.ToString(), teammate5.Content.ToString()), teamListBox.SelectedItem.ToString());
+                    teamListBox.SelectedItem = teamname.Text;
+                    teamListBox.ItemsSource = Team.GetTeamList();
+
+                }
+
 
             }
-            else
-                lblMessage.Content = (a / b).ToString();
         }
-
-        private void Button_Click_5(object sender, RoutedEventArgs e)
-        {
-            lblMessage.Content =  Math.Pow(a, b);
-        }
-
-        // Создать кнопочный калькулятор с действиями + - / * 
     }
 }
---
+++ ITParkWPF/User.cs
@@ -0,0 +1,70 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITParkWPF
+{
+    internal class User
+    {
+        public User(string login, string name, string email, string phoneNumber)
+        {
+            Name = name;
+            Login = login;
+            Email = email;
+            PhoneNumber = phoneNumber;
+        }
+        [BsonIgnoreIfDefault]
+        public ObjectId _id { get; set; }
+        public string Name { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public static void AddToDB(string name, string login, string email, string phone)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Registration");
+            var collection = database.GetCollection<User>("Users");
+            collection.InsertOne(new User(login, name, email, phone));
+        }
+
+        public static List<string> GetLoginList()
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Registration");
+            var collection = database.GetCollection<User>("Users");
+            var listUsersFromDB = collection.Find(x => true).ToList();
+            List<string> listToReturn = new List<string>();
+            foreach (var item in listUsersFromDB)
+            {
+                listToReturn.Add(item.Login);
+            }
+            return listToReturn;
+        }
+        public static User GetUser(string name)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Registration");
+            var collection = database.GetCollection<User>("Users");
+            var foundedUser = collection.Find(x => x.Login == name).FirstOrDefault();
+            return foundedUser;
+        }
+
+        public static void ReplaseUser(string nameFromList, User updated)
+        {
+            var client = new MongoClient("mongodb://localhost");
+            var database = client.GetDatabase("Registration");
+            var collection = database.GetCollection<User>("Users");
+            collection.ReplaceOne(x => x.Login == nameFromList, updated);
+        }
+
+    }
+
+    // Реализовать кнопку редактирования уже существующего пользователя
+}
+
